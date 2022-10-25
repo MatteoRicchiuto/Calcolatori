c3d551371362f9de78d3b1b53e587aa9268c18a2
MatteoRicchiuto <116072651+MatteoRicchiuto@users.noreply.github.com>
2022-10-25 20:17:17 +0200

    Update Lecture_1.py

--- Lecture 1/Lecture_1.py
+++ Lecture 1/Lecture_1.py
@@ -5,7 +5,7 @@ L_parola = len(parola)
 i = 1
 N_vocali = 0
 vocali = "aeiouyAEIOUY"
-print()
+
 print("La parola " + parola + " è lunga " + str(L_parola) + " lettere")
 
 while i < L_parola:
@@ -26,13 +26,30 @@ L_parola = len(parola)
 i = 1
 N_vocali = 0
 vocali = "aeiouyAEIOUY"
-print()
+
 print("La parola " + parola + " è lunga " + str(L_parola) + " lettere")
 
 while i < L_parola:
     if i % 2 == 0 and parola[i] in vocali:
          N_vocali += 1
     i = i + 1
+
+print("La parola " + parola + " è composta da " + str(N_vocali) + " vocali.")
+
+
+
+
+# In[] Conta Vocali Pare V3 (ciclo for)
+parola = input("Inserisci una parola: ")
+L_parola = len(parola)
+
+N_vocali = 0
+
+print("La parola " + parola + " è lunga " + str(L_parola) + " lettere")
+
+for c in parola:
+    if c in "AEIOUYaeiouy":
+        N_vocali += 1
 
 print("La parola " + parola + " è composta da " + str(N_vocali) + " vocali.")
 
@@ -46,7 +63,7 @@ L_parola = len(parola)
 N_vocali = 0
 sottolineatura = ""
     
-print()
+
 print("La parola " + parola + " è lunga " + str(L_parola) + " lettere")
 
 for c in parola:
