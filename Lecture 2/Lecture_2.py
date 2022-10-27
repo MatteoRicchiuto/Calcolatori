481d6a4f6b5d0f28612404b1f72821509fbaaca4
MatteoRicchiuto <116072651+MatteoRicchiuto@users.noreply.github.com>
2022-10-27 13:21:50 +0200

    esercizzio 5

--- Lecture 2/Lecture_2.py
+++ Lecture 2/Lecture_2.py
@@ -1,14 +1,14 @@
 # In[] Slicing
 x = input("inserisci una stringa: ")
 
-x0 = x[1:4] # le lettere dalla 1 alla 4
-x1 = x[:4] # lettere dalla prima alla 4
-x2 = x[3:] #lettere dalla 3 all'ultima 
-x3 = x[:] #tutte le letterere
-x4 = x[3:8:2] #le lettere tra 3 e 8 ma con intervalli di due
-x5 = x[6:1:-1] #le letter dalla 6 alla 1 ma in ordine inverso
-x6 = x[::-1] #inverte tutta la stringa
-
+x0 = x[1:4]    # le lettere dalla 1 alla 4
+x1 = x[:4]     # lettere dalla prima alla 4
+x2 = x[3:]     # lettere dalla 3 all'ultima 
+x3 = x[:]      # tutte le letterere
+x4 = x[3:8:2]  # le lettere tra 3 e 8 ma con intervalli di due
+x5 = x[6:1:-1] # le letter dalla 6 alla 1 ma in ordine inverso
+x6 = x[::-1]   # inverte tutta la stringa
+x7 = x[::2]    # 2 si chiama step
 
 print(x0)
 print(x1)
@@ -17,3 +17,4 @@ print(x3)
 print(x4)
 print(x5)
 print(x6)
+print(x7)
--- Lecture 3/Exercise3.py
+++ Lecture 3/Exercise3.py
@@ -1,3 +1,29 @@
 '''
-progettare una funzione che prende in input due interi a e b con b > a e restituisce a*(a+1)*(a+2)*..(b-1)*b
+Esercizio per casa:
+      Scrivere una funzione che prende in input due stringhe e restituisce il carattere della prima stringa che compare più volte nella seconda. Ad esempio se la prima stringa è ciao e la seconda è ramarro marrone, la funzione deve restituire a.
 '''
+
+'''
+giovanni 
+c = i
+ 
+cannibbale
+
+i 1
+a 2
+n 2
+'''
+
+def lettera_pp (s1, s2):
+
+    lettera = 0
+    numero = 0
+
+    for c in s1:
+        if c in s2:
+            for d in s2:
+                if c = d:
+                    numero =+ 1
+            
+
+
