4a67da1d1932b8a6d1d49c144b402c1f71c40cbf
MatteoRicchiuto <116072651+MatteoRicchiuto@users.noreply.github.com>
2022-10-29 10:50:24 +0200

    Update Exercise3.py

--- Lecture 3/Exercise3.py
+++ Lecture 3/Exercise3.py
@@ -1,38 +1,40 @@
 '''
 Esercizio per casa:
-      Scrivere una funzione che prende in input due stringhe e restituisce il carattere della prima stringa che compare più volte nella seconda. Ad esempio se la prima stringa è ciao e la seconda è ramarro marrone, la funzione deve restituire a.
-'''
-
-'''
-giovanni 
-c = i
- 
-cannibbale
-
-i 1
-a 2
-n 2
+      Scrivere una funzione che prende in input due stringhe e restituisce il carattere della prima stringa che compare più volte nella seconda. 
+      Ad esempio se la prima stringa è ciao e la seconda è ramarro marrone, la funzione deve restituire a.
 '''
 
 def lettera_pp (s1, s2):
 
-    lettera = "n"
-    numero1 = 0
-    numero2 = 0
+    lettera = "*"
+    numero_test = 0
+    numero_mem = 0
+
 
     for c in s1:
+        numero_test = 0
         if c in s2:
             for d in s2:
                 if c == d:
-                    numero1 =+ 1
-                if numero2 > numero1:
-                    numero1 = numero2
+                    numero_test += 1
+                if numero_test > numero_mem:
+                    numero_mem = numero_test
                     lettera = c
-    print(lettera)
-    print(numero1)
+    
+    if lettera == "*":
+        print("Nessun carattere di " + s1 + " si ripetecazzo in " + s2 + ".")
+    else:
+        if numero_mem > 1:
+            print("Il carattere di " + s1 + " che compare più volte in " + s2 + " è " + lettera + ", si ripete " + str(numero_mem) + " volte.")
+        else:
+            print("Il carattere di " + s1 + " che compare più volte in " + s2 + " è " + lettera + ", si ripete " + str(numero_mem) + " volta.")
 
-a = "giovanni"
-b = "cannibbale"
+
+
+
+a = input("Inserisci la prima parola: ")
+b = input("Inserisci la seconda parola: ")
+
 
 lettera_pp(a, b)
 
