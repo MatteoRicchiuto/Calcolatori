f522f99d216c7e01860f2aaa5e1698ee60e25061
MatteoRicchiuto <116072651+MatteoRicchiuto@users.noreply.github.com>
2022-11-23 13:59:25 +0100

    cioa

--- Lecture 11/Lecture11.py
+++ Lecture 11/Lecture11.py
@@ -19,7 +19,7 @@ print( sum(a))          # Somma di tutti i numeri
     # Questi operatori funzionano soltanto se sono utilizati tra valori comparabili tra di loro
 
 # In[] Aliasing e clonazione liste
-a = [10,10,5,5]
+a = [10,10,5,5] ciao
 
 # clonazione
 b = a[:]
