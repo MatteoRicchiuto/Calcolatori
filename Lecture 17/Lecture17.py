3d8089e2b83b5298b7f5cef507d0574c06c66b24
MatteoRicchiuto <116072651+MatteoRicchiuto@users.noreply.github.com>
2022-12-07 20:27:39 +0100

    Lecture18

--- Lecture 17/Lecture17.py
+++ Lecture 17/Lecture17.py
@@ -1,25 +1,31 @@
 # In[] Ricerca binaria 
+def bin_search( k, bins ):
+    '''
+    sia n-1 la lunghezza di bins, ritorna 0 se k < bins[0],
+        n se k >= bin[n-2], i se bins[i-1] <= k < bin[i]
+    '''
 
-def bin_search(k, bins)
-    '''
-    sia n-1 la lunghezza di bins, 
-    ritorna: 
-            0 se k < bins[0],
-            n se k >= bin[n-2]
-            i se bins[i-1] <= k < bin[i]
-    '''
-    lx, rx = 0, n-1
     n = len(bins)+1
     
-    while True:            # Ciclo teoricamente infinito
-        cx = (lx + rx)//2
+    if k < bins[0]:
+        return 0
+    if k >= bins[n-2]:
+        return n-1
+    
+    lx, rx = 0, n
+    trovato = False
+    
+    while not trovato:
+        cx = (lx+rx)//2
         # cx è il segmento mediano tra lx e rx
         if k >= bins[cx-1] and k < bins[cx]:
-            return cx
-        
-        elif k < bins[cx-1]:
+            trovato = True
+        elif  k < bins[cx-1]:
             # a sx del segmento cx
-            rx = cx -1
+            rx = cx-1
+        else:
+            lx = cx+1
+    
+    return cx
 
-        else: 
-            lx = cx + 1''''0'0'''' 
+print( bin_search(9, [6, 8, 10] ) )
--- Lecture18.py
+++ Lecture18.py
@@ -1,42 +1,28 @@
-def list_to_str(word):
-    for i in word:
-        print(i,end=" ")
-    print("\n")    
-    
-print("Gioco dell' impiccato")
+# Un dizionario è una rcaccolta di elementoi che sono identi ficati:
+# da un valore e da una chiave.
+# In[] Dizionario
+d0 = {} # Dizionario vuoto
+d1 = {"k0": 6, "k1":"python", 6:3.12} 
 
-secret_word = input("Digita la parola segreta: ")  # Successivamente prendere la parola in modo randomico da un file 
 
-semi_word = ["_"] * len(secret_word) # La parola che verra stampata di volta in volta
- 
-                    # solo con le lettere indovinate ( al posto giusto )
-                    
-list_to_str(semi_word) # controllo
+## Lettura
 
-vite = 6 # Testa Corpo x2_Braccio x2_Gambe
+print(d1)           # Stampa tutto il dizionario 
+print(d1["k1"])     # Stampa il valore associato alla chiave (k1)
+#print(d1["k9"])    #! Errore non è una ciave del dizionario
 
-while vite > 0 and semi_word.count("_") != 0:
-    
-    lettera = input("Inserisci una lettera: ")
-    
-    
-        
-        
-    if lettera in secret_word:
-        
-        for i in range(len(secret_word)):
-            if lettera == secret_word[i]:
-                
-                semi_word[i] = lettera
-        list_to_str(semi_word)
-    else:
-        print("Lettera sbagliata")
-        vite -= 1
-        print("vite rimanenti: ", vite)
-            
-if vite:
-    print("Bravo hai vinto :)")
-else:
-    print("Hai perso :(")
-    
+## Scrittura
 
+d1["k0"] = True     # Sovrascrittura 
+d1["L"] = False
+
+print(d1)
+
+## Cancellazione
+
+del(d1["k0"])
+#del(d1["python"]) . #! Errore: python non è una delle chiavi
+
+print(d1)
+
+# %%
